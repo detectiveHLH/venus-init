927e12401501224038eb39b4d8c1cc03da502ec3
hulunhao <545759184@qq.com>
2019-04-17 09:45:27 +0800

    Finish replace all project name and project group id

--- action/initProject.js
+++ action/initProject.js
@@ -3,27 +3,69 @@ const shell = require('shelljs');
 const { log, mkdir, cd, cp, rm } = require('./util');
 
 /**
- * 初始化项目
+ * Initial project
  */
 const initProject = (config) => {
   const { projectName } = config;
 
+  /**
+   * Clone the demo project from github
+   * source: https://github.com/detectiveHLH/venus-demo
+   */
   log('Initialing the project......');
   shell.exec('git clone https://github.com/detectiveHLH/venus-demo.git ' + config.projectName);
+
+  /**
+   * Into the main directory and remove the git directory
+   */
   cd(projectName);
   rm('.git');
-  // // 创建项目主目录
-  // if (mkdir(config.projectName)) {
-  //   log(`The main directory of this project named ${config.projectName} has been created.`, 'green');
-  //
-  //   log('Creating api module......');
-  //   cd(config.projectName);
-  //   if (mkdir('api')) {
-  //     log(`Api created`, 'green');
-  //     cd('api');
-  //   }
-  // }
+
+  /**
+   * - Replace file in root directory
+   */
+  shell.ls('*.xml').forEach(file => {
+    shell.sed('-i', 'PROJECT_NAME', projectName, file);
+    shell.sed('-i', 'PROJECT_GROUP_ID', convertToDot(projectName), file);
+  });
+
+  /**
+   * Start replace in api module
+   */
+  cd('api');
+
+  // Replace parameter in api pom.xml
+  shell.ls('*.xml').forEach(file => {
+    shell.sed('-i', 'PROJECT_NAME', projectName, file);
+    shell.sed('-i', 'PROJECT_GROUP_ID', convertToDot(projectName), file);
+  });
+
+  cd('src/main/java/com/t4f/web');
+  shell.exec('ls');
+
+
+
+  /**
+   * - Return to root directory
+   */
+  cd('../../../../../../../');
+
+  /**
+   * - Into shared directory
+   */
+  cd('shared');
+  shell.ls('*.xml').forEach(file => {
+    shell.sed('-i', 'PROJECT_NAME', projectName, file);
+    shell.sed('-i', 'PROJECT_GROUP_ID', convertToDot(projectName), file);
+  });
+
 };
 
+/**
+ * Convert '-' to '.'
+ */
+const convertToDot = (s) => {
+  return s.replace(/-/g, '.');
+};
 
 module.exports = initProject;
